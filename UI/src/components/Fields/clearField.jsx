aecca00c706bee8b3a87c50d76667aeaba894080
alexander.dick <alexander.dick@adesso.at>
2022-07-28 12:43:07 +0200

    changed color of button

--- UI/src/components/Fields/clearField.jsx
+++ UI/src/components/Fields/clearField.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ClearField extends Component{
     render() { 
         return (
-            <input className='col btn btn-sm btn-primary m-1' type='button' onClick={() => this.props.onClear()} value={this.props.value} />
+            <input className='col btn btn-sm btn-danger m-1' type='button' onClick={() => this.props.onClear()} value={this.props.value} />
         );
     }
 }
